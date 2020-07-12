9f787308a0634bfc28647bf790e8ca048b30c078
Pierre Ravenel <pierreafe@gmail.com>
2020-07-12 14:00:45 +0200

    minimal network with genetic evolution

---
+++ testdl.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+
+nb_iterarion = 5000
+family_size = 10
+learning_rate = 0.001
+
+class Network:
+    """ CNN """
+    def __init__(self):
+        self.D_in = 36   # input dimension
+        self.N = 1       # batch size
+        self.H = 100     # hidden dimension
+        self.D_out = 36  # output dimension
+
+        # Randomly initialize weights
+        self.w1 = np.random.randn(self.D_in, self.H)
+        self.w2 = np.random.randn(self.H, self.D_out)
+
+        # in and out data
+        self.x = 0
+        self.y_pred = 0
+
+    def forward(self, x):
+        # Forward pass: compute predicted y
+        self.x = x
+        h = self.x.dot(self.w1)
+        h_relu = np.maximum(h, 0)
+        self.y_pred = h_relu.dot(self.w2)
+        return self
+
+    def loss(self, y):
+        return np.square(self.y_pred - y).sum()
+
+    def compute_score(self, x, y):
+        return self.forward(x).loss(y)
+
+    def derivate(self, learning_rate):
+        div = Network()
+        div.w1 = self.w1 + np.random.randn(self.D_in, self.H) * learning_rate
+        div.w2 = self.w2 + np.random.randn(self.H, self.D_out) * learning_rate
+        return div
+
+
+# Create random input and output data
+x = np.random.randn(1, 36)
+y = np.random.randn(1, 36)
+
+best_network = Network()  # random network
+
+last_best_loss = float('inf')
+
+for t in range(nb_iterarion):
+    # create family
+    family = [best_network.derivate(learning_rate) for _ in range(family_size)]
+    # compute best network
+    best_network = min(family, key=lambda net: net.forward(x).loss(y))
+    # debug
+    best_loss = best_network.loss(y)
+    delta = last_best_loss - best_loss
+    print(f"[{t}/{nb_iterarion}]" +
+          "score={:.5f}\t".format(best_loss) +
+          ("\33[92m+" if delta > 0 else "\33[91m") + str(delta) + "\33[39m",
+          end='\r')
+    last_best_loss = best_loss
