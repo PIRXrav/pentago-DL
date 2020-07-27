5ff0959bdbe7290f923000446621685aff39d402
Pierre Ravenel <pierreafe@gmail.com>
2020-07-27 14:58:42 +0200

    ...

--- trainer.py
+++ trainer.py
@@ -20,6 +20,7 @@ class Trainer():
         self.score = 0  # nb win vs witness
         # output
         self.winrates = []
+        self.done = False
 
     def iter(self, family_size, learning_rate):
         """ exec 1 step """
@@ -50,6 +51,8 @@ class Trainer():
         self.winrates.append(self.network.winrate())
         # debug
         self.iter_debug()
+        # done ?
+        self.done = self.network.winrate() == 1
         # reset scores
         for network in chain(self.witness_tab, [self.network]):
             network.reset_score()
@@ -81,17 +84,34 @@ class Trainer():
         plt.legend()
         plt.show()
 
-
-if __name__ == '__main__':
-
+def compute_nw_with_lvl(n):
+    """
+    fonction récursive qui calcule un réseau de niveau n.
+    1 niveau inque que le réseau à battu k réseau de rang n-1
+    """
+    k = 10
     family_size = 10
     learning_rate = 0.01
-    nb_iterarion = 100
+    # nb_iterarion = 100
+    if n == 0:
+        return NetworkPentago()
 
-    witnesss = [NetworkPentago() for _ in range(10)]
-    best = NetworkPentago()
-    trainer = Trainer(best, witnesss)
-    for _ in range(nb_iterarion):
+    print(f"Create family {n}")
+    witnesss = [compute_nw_with_lvl(n-1) for _ in range(k)]
+    trainer = Trainer(NetworkPentago(), witnesss)
+    while not trainer.done:
         trainer.iter(family_size, learning_rate)
         print('Trainer score: ', trainer.score)
-    trainer.plot_loss()
+    return trainer.network
+
+if __name__ == '__main__':
+    # compute best nw
+    best_nw = compute_nw_with_lvl(2)
+    print(best_nw)
+
+    # test best_nw
+    print("TEST")
+    best_nw.reset_score()
+    for _ in range(1000):
+        best_nw.play(NetworkPentago())
+    print(f"WINRATE : {best_nw.winrate()}")
